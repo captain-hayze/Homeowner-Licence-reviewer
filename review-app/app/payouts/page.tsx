e59a48e06144d00809e1670b00c5b337c1c0989e
danny <danielenagu@gmail.com>
2026-06-01 23:06:24 +0100

    Endpoint integration

--- review-app/app/payouts/page.tsx
+++ review-app/app/payouts/page.tsx
@@ -1,13 +1,14 @@
 "use client"
-import { useState, useCallback, useEffect } from "react";
+import dayjs from "dayjs";
+import useSWR from "swr";
+import { useState, useCallback, useEffect, useMemo } from "react";
 import ReviewerLayout from "../../components/layouts/ReviewerLayout";
-import { Card, Table, Button, Tag, Radio, Spin, message } from "antd";
+import { Card, Table, Button, Tag, Radio, Spin, message, Skeleton } from "antd";
 import AddAccountModal from "../../components/payouts/AddAccountModal";
 import useSWRMutation from "swr/mutation";
-import { handleMutation } from "@/utils/axios";
+import { handleMutation, fetcher } from "@/utils/axios";
 import { useCurrency } from "@/hooks/useCurrency";
 import { useAppStore } from "@/providers/store-provider";
-import dayjs from "dayjs";
 
 const mockPayouts = [
   {
@@ -70,12 +71,18 @@ export default function PayoutsPage() {
   const { user } = useAppStore((state) => state);
   const [open, setOpen] = useState(false);
   const { formatCurrency } = useCurrency();
-  const [accounts] = useState<PayoutAccount[]>(defaultAccounts);
   const [activeAccount, setActiveAccount] = useState<PayoutAccount | null>(defaultAccounts[0]);
+
+  const { data, isLoading } = useSWR("/license-user/all-settlement-accounts", fetcher);
+
   const { trigger: activateAccount, isMutating: isActivating } = useSWRMutation(
     `license-user/${activeAccount?.id}/activate-settlement-account`,
     handleMutation,
   );
+
+  const accounts: PayoutAccount[] = useMemo(() => {
+    return data?.data || [];
+  }, [data]);
 
   const handleActivateAccount = useCallback(async() => {
     if (!activeAccount) return;
@@ -121,30 +128,39 @@ export default function PayoutsPage() {
 
         <Card title="Payment accounts">
           <div className="grid gap-4 sm:grid-cols-2">
-            {accounts.map((account) => (
-              <Card key={account.accountNumber} size="small" className="border-gray-200 shadow-sm">
-                <div className="flex items-start gap-3">
-                {activeAccount?.id === account.id && isActivating 
-                  ? (<Spin className="animate-spin" />)
-                  : (<Radio
-                      defaultChecked={account.isActive}
-                      value={account}
-                      name={account.id}
-                      onChange={() => setActiveAccount(account)}
-                    />
-                  )}
-                  <div className="flex-1 flex flex-col gap-3">
-                    <div className="text-base font-semibold">{account.accountName}</div>
-                    <div className="text-sm text-gray-500">{account.bankName}</div>
-                    <div className="text-sm text-gray-500">{account.accountNumber}</div>
-                    {/* <Tag color={account.accountType === "bank" ? "blue" : "green"}>
-                      {account.accountType === "bank" ? "Bank" : "Mobile wallet"}
-                    </Tag> */}
-                  </div>
-                </div>
-              </Card>
-            ))}
+            {isLoading
+              ? <Skeleton />
+              : (<>
+                {accounts.map((account) => (
+                  <Card key={account.accountNumber} size="small" className="border-gray-200 shadow-sm">
+                    <div className="flex items-start gap-3">
+                    {activeAccount?.id === account.id && isActivating 
+                      ? (<Spin className="animate-spin" />)
+                      : (<Radio
+                          defaultChecked={account.isActive}
+                          value={account}
+                          name={account.id}
+                          onChange={() => setActiveAccount(account)}
+                        />
+                      )}
+                      <div className="flex-1 flex flex-col gap-3">
+                        <div className="text-base font-semibold">{account.accountName}</div>
+                        <div className="text-sm text-gray-500">{account.bankName}</div>
+                        <div className="text-sm text-gray-500">{account.accountNumber}</div>
+                        {/* <Tag color={account.accountType === "bank" ? "blue" : "green"}>
+                          {account.accountType === "bank" ? "Bank" : "Mobile wallet"}
+                        </Tag> */}
+                      </div>
+                    </div>
+                  </Card>
+                ))}
+              </>)}
           </div>
+          {!isLoading && accounts.length === 0 && (
+            <div className="text-center text-gray-500 py-10">
+              No payout accounts found. Please add an account to receive payouts.
+            </div>
+          )}
         </Card>
 
         <Card title="Past payout requests">
